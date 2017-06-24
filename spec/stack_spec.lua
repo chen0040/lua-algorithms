3d72eed199ce847c52de1b288260cd2fd9af77e7
xschen <xs0040@gmail.com>
2017-06-24 22:44:10 +0800

    update api call

--- spec/stack_spec.lua
+++ spec/stack_spec.lua
@@ -5,8 +5,10 @@ describe('stack', function()
   describe('stack.LinkedListStack()', function()
     it('should push and pop items', function()
       local s = stack.LinkedListStack.create();
-      s.push(1);
-      assert.equal(s.pop(), 1)
+      s:push(1);
+      print(s:size())
+      assert.equal(s:size(), 1)
+      assert.equal(s:pop(), 1)
     end)
   end)
 
