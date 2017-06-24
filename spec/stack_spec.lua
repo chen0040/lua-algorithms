43cfd7475918dca089592e1fb1d2f37f67b420c1
xschen <xs0040@gmail.com>
2017-06-24 23:34:17 +0800

    more coverage and unit testing for the linked list stack

--- spec/stack_spec.lua
+++ spec/stack_spec.lua
@@ -6,9 +6,16 @@ describe('stack', function()
     it('should push and pop items', function()
       local s = stack.LinkedListStack.create();
       s:push(1);
+      s:push(2);
+      s:push(3);
       print(s:size())
-      assert.equal(s:size(), 1)
+      assert.equal(s:size(), 3)
+      assert.equal(s:isEmpty(), false)
+      assert.equal(s:pop(), 3)
+      assert.equal(s:pop(), 2)
       assert.equal(s:pop(), 1)
+      assert.equal(s:size(), 0)
+      assert.equal(s:isEmpty(), true)
     end)
   end)
 
